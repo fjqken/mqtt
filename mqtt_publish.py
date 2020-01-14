3f46b1455bc5fabed061f28ed26abeb965e14352
fung <469965307@qq.com>
2020-01-15 01:35:35 +0800

    test

--- mqtt_publish.py
+++ mqtt_publish.py
@@ -1,16 +1,19 @@
 import paho.mqtt.publish as publish
 import time
 from hashlib import md5
+import json
 
-def mqtt_Publish(topic,msg,username,password):
+
+def mqtt_Publish(topic, msg, username, password):
     client_id = "X:DEVICE;A:2;V:1;"
     IP_DNS = "mqtt.xlink.cn"
     port = 1883
-    publish.single(topic,msg,qos=0,
+    publish.single(topic, msg, qos=1,
                    hostname=IP_DNS,
                    port=port,
                    client_id=client_id,
                    auth={'username': username, 'password': password})
+
 
 def encrypt_md5(s):
     # 创建md5对象
@@ -20,9 +23,12 @@ def encrypt_md5(s):
     # 加密
     return new_md5.hexdigest()
 
+
 if __name__ == '__main__':
     subtopic = "$xlink/device/activation"
     client_id1 = "X:DEVICE;A:2;V:1;"
     pid = "160002baec9203e9160002baec92c601"
     pkey = "7c74dc459c1b55926184df2f3bd29d65"
-    password1 = (encrypt_md5(pid+pkey))
+    message1 = json.dumps({"Product ID": pid, "mac": "AAA1"})
+    password1 = (encrypt_md5(pid + pkey))
+    mqtt_Publish(subtopic, message1, pid, pkey)
--- test.py
+++ test.py
@@ -18,4 +18,39 @@ import json
 from mqtt_publish import mqtt_Publish
 from time import sleep
 from mqtt_subscribe import Mqtt_subscribe
+from hashlib import md5
 import datetime
+
+
+def encrypt_md5(s):
+    # 创建md5对象
+    new_md5 = md5()
+    # 这里必须用encode()函数对字符串进行编码，不然会报 TypeError: Unicode-objects must be encoded before hashing
+    new_md5.update(s.encode(encoding='utf-8'))
+    # 加密
+    return new_md5.hexdigest()
+
+
+class Activation():
+    def __init__(self):
+        print("init")  # never prints
+
+    pid = "160002baec9203e9160002baec92c601"
+
+    def activation_device(self):
+        subtopic = "$xlink/device/activation"
+        client_id1 = "X:DEVICE;A:2;V:1;"
+        pid = self.pid
+        pkey = "7c74dc459c1b55926184df2f3bd29d65"
+        message1 = json.dump({"product_id": pid, "mac": "AAA1" })
+        print(pid + pkey)
+        password1 = (encrypt_md5(pid + pkey))
+        print(password1)
+        print(client_id1)
+        mess = Mqtt_subscribe(subtopic, client_id1, pid, password1)
+        mess.start()
+        mqtt_Publish(subtopic, message1, pid, pkey)
+
+
+a =Activation()
+a.activation_device
